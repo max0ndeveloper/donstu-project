d2a9ceb9654bf9124fe0912c0c9999a18630197c
max0ndeveloper <maxim2003s@gmail.com>
2021-09-12 22:31:54 +0300

    create map function to display content

--- src/Components/WebSecurityContent/WebSecurityContent.js
+++ src/Components/WebSecurityContent/WebSecurityContent.js
@@ -1,10 +1,66 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./WebSecurityContent.css"
+import useFetch from "../../Hooks/useFetch";
 
 
-export const WebSecurityContent = () => {
+const WebSecurityContent = () => {
+  const url = '/articles'
+  const [{isLoading, response, error}, doFetch] = useFetch(url);
+  useEffect(() => {
+    doFetch()
+  }, [doFetch])
+
+
+
+  // console.log('data', isLoading, response, error)
+
+  const spinnerJSX = isLoading ? <div>Идёт загрузка</div> : null
+
+  const articleJSX = (articles) => {
+    return articles.map((article, index) => {
+      return(
+          <div key={index}>
+            <div>
+              {article.value}
+            </div>
+          </div>
+      )
+    })
+  }
+
+  const contentJSX = response ? response.map(function(item, index) {
+    return(<div key={index}>
+      <div>
+        {item.header}
+      </div>
+      <div>
+        {
+        console.log('item', item)
+        }
+
+        {articleJSX(item.contents)}
+
+        {/*{*/}
+        {/*  item.content.map(function (item, index) {*/}
+        {/*    return(*/}
+        {/*        <div key={index}>*/}
+        {/*          <div>*/}
+        {/*            {item.value}*/}
+        {/*          </div>*/}
+        {/*        </div>*/}
+        {/*    )*/}
+        {/*  })*/}
+        {/*}*/}
+      </div>
+    </div>)
+  }) : null
+
+
+
   return (
       <div className="container">
+        {spinnerJSX}
+        {contentJSX}
         <div>
           <p>First card </p>
         </div>
@@ -14,3 +70,5 @@ export const WebSecurityContent = () => {
       </div>
   )
 }
+
+export default WebSecurityContent;
